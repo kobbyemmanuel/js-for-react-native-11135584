36cc2635aa6af88fb8d55f5ecab6b489ddf0d0a9
kobbyemmanuel <kobypoku4@gmail.com>
2024-05-19 20:21:09 +0000

    changes made in task 1 completed(task 2)

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -6,7 +6,7 @@ function processArray(arr) {
     for (let i = 0; i < arr.length; i++) {
        
         if (arr[i] % 2 === 0) {
-           
+            
             result.push(arr[i] * arr[i]);
         } else {
             
@@ -19,6 +19,36 @@ function processArray(arr) {
 }
 
 
+function formatArrayStrings(stringsArray, numbersArray) {
+    
+    if (stringsArray.length !== numbersArray.length) {
+        throw new Error("Arrays must have the same length.");
+    }
+
+    
+    let formattedArray = [];
+    
+    
+    for (let i = 0; i < stringsArray.length; i++) {
+        
+        if (numbersArray[i] % 2 === 0) {
+            
+            formattedArray.push(stringsArray[i].toUpperCase());
+        } else {
+            
+            formattedArray.push(stringsArray[i].toLowerCase());
+        }
+    }
+    
+    
+    return formattedArray;
+}
+
+
 const inputArray = [1, 2, 3, 4, 5];
 const processedArray = processArray(inputArray);
 console.log(processedArray); 
+
+const stringsArray = ["apple", "Banana", "cherry", "DURIAN", "Elephant"];
+const formattedStringsArray = formatArrayStrings(stringsArray, processedArray);
+console.log(formattedStringsArray); 
